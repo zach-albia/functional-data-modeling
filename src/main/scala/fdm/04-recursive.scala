6ec916fcdde8652bed0e3d32b4990682ce8367ac
zach.albia <zach.albia@gmail.com>
2021-01-11 15:04:09 +0800

    do exercise 4

--- src/main/scala/fdm/04-recursive.scala
+++ src/main/scala/fdm/04-recursive.scala
@@ -2,6 +2,8 @@ package fdm
 
 package fdm
 
+import scala.annotation.tailrec
+
 /**
  * Scala data types constructed from enums and case classes may be *recursive*: that is, a top-
  * level definition may contain references to values of the same type.
@@ -14,7 +16,7 @@ object recursive {
    * Create a recursive data type that models a user of a social network, who has friends; and
    * whose friends may have other friends, and so forth.
    */
-  final case class User()
+  final case class User(name: String, friends: Set[() => User])
 
   /**
    * EXERCISE 2
@@ -24,7 +26,10 @@ object recursive {
    */
   sealed trait NumericExpression
   object NumericExpression {
-    final case class Literal(value: Int) extends NumericExpression
+    final case class Literal(value: Int)                                                    extends NumericExpression
+    final case class Addition(addend1: NumericExpression, addend2: NumericExpression)       extends NumericExpression
+    final case class Multiplication(factor1: NumericExpression, factor2: NumericExpression) extends NumericExpression
+    final case class Subtraction(minuend: NumericExpression, subtrahend: NumericExpression) extends NumericExpression
   }
 
   /**
@@ -36,6 +41,7 @@ object recursive {
   sealed trait EmailTrigger
   object EmailTrigger {
     case object OnPurchase                                         extends EmailTrigger
+    case object OnShoppingCartAbandonment                          extends EmailTrigger
     final case class Both(left: EmailTrigger, right: EmailTrigger) extends EmailTrigger
   }
 }
@@ -55,7 +61,7 @@ object cyclically_recursive {
    * Create a snake that is eating its own tail. In order to do this, you will have to use a
    * `lazy val`.
    */
-  val snake: Snake = ???
+  lazy val snake: Snake = Snake(snake)
 
   /**
    * EXERCISE 2
@@ -64,7 +70,10 @@ object cyclically_recursive {
    * the `coworker` field from `Employee` to `() => Employee` (`Function0`), also called a "thunk",
    * and you will have to use a `lazy val` to define the employees.
    */
-  final case class Employee(name: String, coworker: Employee)
+  final case class Employee(name: String, coworker: () => Employee)
+
+  lazy val tim: Employee = Employee("Tim", () => tom)
+  lazy val tom: Employee = Employee("Tom", () => tim)
 
   /**
    * EXERCISE 3
@@ -73,12 +82,28 @@ object cyclically_recursive {
    * itself.
    */
   sealed trait LazyList[+A] extends Iterable[A]
+
   object LazyList {
-    def apply[A](el: A): LazyList[A] = ???
+    case object LazyNil extends LazyList[Nothing] { def iterator = Iterator.empty[Nothing] }
+    case class LazyCons[A](a: () => A, tl: () => LazyList[A]) extends LazyList[A] {
+      def iterator = Iterator.single(a()) ++ tl().iterator
+    }
+
+    def apply[A](el: A): LazyList[A] = LazyCons(() => el, () => LazyNil)
+
+    def empty[A]: LazyList[A] = LazyNil
 
     // The syntax `=>` means a "lazy parameter". Such parameters are evaluated wherever they are
     // referenced "by name".
-    def concat[A](left: => LazyList[A], right: => LazyList[A]): LazyList[A] = ???
+    def concat[A](left: => LazyList[A], right: => LazyList[A]): LazyList[A] =
+      left match {
+        case LazyNil => right
+        case LazyCons(a, tl) =>
+          tl() match {
+            case LazyNil         => LazyCons(a, () => right)
+            case LazyCons(b, tl) => LazyCons(a, () => LazyCons(b, () => concat(tl(), right)))
+          }
+      }
   }
 
   lazy val infiniteList: LazyList[Int] = LazyList.concat(LazyList(1), infiniteList)
